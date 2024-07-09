2fc1aa105a58ac7d71e8ed80b2baeed4ce62d379
PaulaNuness <127285238+PaulaNuness@users.noreply.github.com>
2024-07-09 19:43:52 +0200

    09-07-2024__19:43

--- POM/patron_page_object_model_POM.py
+++ POM/patron_page_object_model_POM.py
@@ -1,2 +1,8 @@
 #Page Object Model es un patrón de diseño que trata de aislar la lógica y elementos de cada página web en una clase Java independiente. Por ejemplo, si un sitio web dispone de una página “Home”, una página “Contactos”, una página “Descargas”…, cada página será aislada en una clase (Home_page.py, Contactos_page.py, Descargas_page.py …) que contendrá los elementos y acciones que podemos realizar en ella
 
+# en las practicas vamos a ejecutar:
+  # irnos a Google
+  # buscar wikipedia
+  # vamos a evaluar el titulo de la pagina
+  # pulsamos en wikipedia
+  # vamos a traer el titulo "Torre del Catalán"
